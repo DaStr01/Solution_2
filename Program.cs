d79a337cfb580ac38a10a743241753b081cbe726
Administrator <Administrator@COMPUTER>
2023-03-13 19:16:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            uint first;
+            int second;
+            double doubleValue;
+            float floatValue;
+            short shortValue;
+            char charValue;
+            string stringValue;
+            ushort ushortValue;
+            bool boolValue;
+            long longValue;
+
+        }
+    }
+}
